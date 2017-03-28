e7e22ffc66ed92010f20fe1aab945d022d85c87e
FiorellaQA <fiorella_90_17@hotmail.com>
2017-03-28 11:36:57 -0800

    añadiendo archivos e implementando validaciones

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Variables
+//VARIABLES
 	function Paciente (nombre, apellido, edad, pais, ciudad, genero){
 		this.nombre = nombre;
 		this.apellido = apellido;
@@ -15,8 +15,61 @@
 	}
 
 	var arrayPacientes = [];
-
-//Eventos
+	var name1 = document.getElementById("name");
+	var lastname = document.getElementById("lastname");
+	var edad = document.getElementById("age");
+	var pais = document.getElementById("country");
+	var ciudad = document.getElementById("city");
+	var genero = document.getElementById("gender");
+//FUNCIONES
+	function soloLetras(e){
+		var codigoTecla = e.keyCode;
+		if((codigoTecla>=97 && codigoTecla<=122)||(codigoTecla>=65 && codigoTecla<=90)||codigoTecla==39 || codigoTecla == 32){
+			this.nextElementSibling.innerHTML = "";
+			return true;
+		}else{
+			this.nextElementSibling.innerHTML = "<br>*Este campo solo permite letras";
+			return false;
+		}
+	}
+	function soloNumeros(e){
+		var codigoTecla = e.keyCode;
+		var longitud = this.value.length;
+		if(codigoTecla>=48 && codigoTecla<=57 && this.value.length<2){
+			this.nextElementSibling.innerHTML = "";
+			return true; 
+		}else{
+			this.nextElementSibling.innerHTML = "<br>*Este campo solo permite números de 2 digitos";
+			return false;
+		}
+		//Pasa al siguiente input
+		// if(longitud==2){
+		// 	this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.focus();
+		// }
+	}
+	function validationInput(){
+		if(this.value.trim().length === 0){
+			this.nextElementSibling.innerHTML = "</br>*Este campo es obligatorio";
+		}else{
+			this.nextElementSibling.innerHTML = "";		
+		}
+		//CONVIERTE LA PRIMERA LETRA DE UNA PALABRA A MAYUSCULA 
+		// var datoCorrecto = this.value.charAt(0).toUpperCase()+this.value.substring(1).toLowerCase();
+		// this.value=datoCorrecto;
+		//CONVIERTE LA PRIMERA LETRA DE 2 O MAS PALABRAS A MAYUSCULA
+		if(this.getAttribute("type")=="text"){
+			var arrDato = this.value.split(" ");
+			var datoConMayusculas = "";
+		for(var i = 0; i<arrDato.length;i++){
+				datoConMayusculas += arrDato[i].charAt(0).toUpperCase() + arrDato[i].substring(1).toLowerCase() + " ";
+		}
+			this.value=datoConMayusculas;
+		}
+		//Limpia el campo indicacion cuando el usuario ya esta escribiendo
+		var indicacion = document.getElementById("indicacion");
+			indicacion.innerHTML = "";
+	}
+//EVENTOS
 	document.getElementById("bt").addEventListener("click",function(e){
 		e.preventDefault();
 		var name = document.getElementById("name").value;
@@ -26,57 +79,35 @@
 		var city = document.getElementById("city").value;
 		var gender = document.getElementById("gender").value;
 		var records = document.getElementById("records");
+		var indicacion = document.getElementById("indicacion");
 		if(name.length !=0 && lastname.length != 0 && age.length != 0 && country.length != 0 && city.length != 0 && gender.length !=0){
 			var nuevoPaciente = new Paciente(name, lastname, age, country, city, gender);
-			arrayPacientes.push(nuevoPaciente); 
-			records.innerHTML += "<div class='paciente'>" + nuevoPaciente.ficha() + "</div>";
+			//arrayPacientes.push(nuevoPaciente); 
+			localStorage.setItem("data",JSON.stringify(nuevoPaciente));
+			//records.innerHTML += "<div class='paciente'>" + nuevoPaciente.ficha() + "</div>";
 			document.getElementById("form1").reset();
 			indicacion.innerHTML = "";
+			window.location = "bienvenido.html";
 		}else{	
-			var indicacion = document.getElementById("indicacion");
+			
 			indicacion.innerHTML = "*Todos los campos son obligatorios.</br>";
 		}
-
-
 	});
-
+	name1.onkeypress=soloLetras;
+	lastname.onkeypress=soloLetras;
+	edad.onkeypress = soloNumeros;
+	pais.onkeypress = soloLetras;
+	ciudad.onkeypress = soloLetras;
+	genero.onkeypress = soloLetras;
 	var array = document.getElementsByClassName("input-registro");
-
 	for(i = 0 ; i < array.length; i++){
 		array[i].addEventListener("blur", validationInput);
 	}
 
-	function validationInput(){
-		if(this.value.trim().length === 0){
-			this.nextElementSibling.innerHTML = "</br>*Este campo es obligatorio";
-		}else{
-			this.nextElementSibling.innerHTML = "";		
-		}
-		var datoCorrecto = this.value.charAt(0).toUpperCase()+this.value.substring(1).toLowerCase();
-		this.value=datoCorrecto;
-		var indicacion = document.getElementById("indicacion");
-			indicacion.innerHTML = "";
 
-	}
-	function soloLetras(e){
-		var codigoTecla = e.keyCode;
-		console.log(codigoTecla);
-		if((codigoTecla>=97 && codigoTecla<=122)||(codigoTecla>=65 && codigoTecla<=90)||codigoTecla==39 || codigoTecla == 32){
-			this.nextElementSibling.innerHTML = "";
-			return true;
-		}else{
-			this.nextElementSibling.innerHTML = "*Este campo solo permite letras";
-			return false;
-		}
-	}
 
-	function soloNumeros(e){
-		var codigoTecla = e.keyCode;
-		var longitud = this.value.length;
-		if(longitud==1){
 
-		}
-	}
+
 
 
 
